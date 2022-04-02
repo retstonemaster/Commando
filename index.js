921435ab48f4e1e7203a262c63838d2b4da13fb9
The_Red_Net <45996791+retstonemaster@users.noreply.github.com>
2022-04-02 20:20:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (fs.existsSync(`${__dirname}/data.json`)) {
     appData = require(`${__dirname}/data.json`);
 }
 
-const server = app.listen(3987);
+const server = app.listen(&{Port});
 const wsServer = new ws.Server({ server: server, path: '/api/ws' });
 
 app.use('/maps', (req, res, next) => {
